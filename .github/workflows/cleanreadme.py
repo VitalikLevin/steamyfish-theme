78a17e69a773aaa1a4123c5ccb1b7e51c0e4326a
Vitaliy Levin <network_worms@mail.ru>
2024-01-06 14:30:55 +0300

    52.2024-01-06 ### Added - Logging system

---
+++ .github/workflows/cleanreadme.py
@@ -0,0 +1,16 @@
+import logging
+import re
+logFile = logging.FileHandler("out/textclean.log")
+consoleOut = logging.StreamHandler()
+logging.basicConfig(handlers=(logFile, consoleOut), level=logging.DEBUG)
+regexp = re.compile("(<div al){1}[\u0000-\uFFFF]+/div>")
+tempFile = open("../../README.md", encoding="utf8").read()
+file = open("../../README.md", "w", encoding="utf8")
+def textclean(text, filR):
+  logging.debug("Entered `textclean`")
+  text = regexp.sub("> Read about changes in [Unreleased section](CHANGELOG.md#Unreleased).", text, count=0)
+  filR.truncate(0)
+  filR.write(text)
+  logging.debug("README was successfully overwriten")
+  filR.close()
+textclean(tempFile, file)
